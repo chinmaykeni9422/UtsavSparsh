53173325bc484c4621a620bbdb47373975440670
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-08 15:04:37 +0530

    home component

---
+++ Client/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <h1 className="bg-green-300 text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
+}
+
+export default Home
