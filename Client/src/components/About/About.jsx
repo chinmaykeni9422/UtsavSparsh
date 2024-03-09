6d94e7e2af85cfbdfee9a2391baeaf2e5bc5c83f
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 15:02:44 +0530

    about component

---
+++ Client/src/components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+      <h1 className="bg-green-300 text-3xl text-center">
+        About
+      </h1>
+    </>
+  )
+}
+
+export default About
