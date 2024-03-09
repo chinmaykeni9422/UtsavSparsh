a2432153159f29823faab06376eb95628d622156
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 13:44:46 +0530

    button component and navbar component

--- Client/src/components/Button.jsx/Button.jsx
+++ Client/src/components/Button.jsx/Button.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function Button({name, onClick, color, tColor}) {
+  
   return (
     <button
         className={`bg-${color}-600 text-${tColor} font-[Poppins] py-2 px-5 rounded md:ml-8 hover:bg-${color}-400 duration-500`}
