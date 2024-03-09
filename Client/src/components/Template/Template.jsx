4a0046d9e7340b83f315fee1e0c736468ee88aa8
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 15:03:36 +0530

    tempalte component

---
+++ Client/src/components/Template/Template.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Tempalte() {
+  return (
+    <>
+      <h1 className="bg-green-300 text-3xl text-center">
+        Template
+      </h1>
+    </>
+  )
+}
+
+export default Tempalte
