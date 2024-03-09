a6f04975dc94dd224fd3261a5b59f5ef79bde3c9
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 15:02:35 +0530

    routes added

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,14 +4,20 @@ import './index.css'
 import {createBrowserRouter, Route, createRoutesFromElements, RouterProvider} from "react-router-dom"
 import Layout from './components/Layout/Layout'
 import Home from './components/Home/Home'
+import Template from './components/Template/Template'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path = '/' element = {<Layout />}>
-      <Route path= '' element = {<Home />} />
-    </Route>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path = '/' element = {<Layout />}>
+        <Route path= '' element = {<Home />} />
+        <Route path= 'Template' element = {<Template />} />
+        <Route path= 'About' element = {<About />} />
+        <Route path= 'Contact' element = {<Contact />} />
+      </Route>
+    )
   )
-)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
