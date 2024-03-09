d33754239139f0f007716ed0fa73717c09d5a0c0
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 15:03:03 +0530

    contact component

---
+++ Client/src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <>
+      <h1 className="bg-green-300 text-3xl text-center">
+        Contact
+      </h1>
+    </>
+  )
+}
+
+export default Contact
