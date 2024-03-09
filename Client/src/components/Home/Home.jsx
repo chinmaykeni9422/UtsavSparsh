ad5ab1804b2a107355e2e1c68fc9006ea40bebbf
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-09 15:03:13 +0530

    home component

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Home() {
   return (
-    <h1 className="bg-green-300 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="bg-green-300 text-3xl text-center">
+        Home
+      </h1>
+    </>
   )
 }
 
