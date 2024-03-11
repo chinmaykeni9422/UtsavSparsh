f96fe8442f29f22397de210415caa5a1f8363561
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-11 09:05:42 +0530

    Footer section added

--- Client/src/components/Layout/Layout.jsx
+++ Client/src/components/Layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Outlet} from 'react-router-dom'
 import NavBar from '../NavBar/NavBar'
+import Footer from '../Footer/Footer'
 
 function Layout() {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Footer />
     </>
   )
 }
