4587ef82c0b05d2215b7baa42d8dca6faaea62e7
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-12 11:13:02 +0530

    added more props

--- Client/src/components/Button.jsx/Button.jsx
+++ Client/src/components/Button.jsx/Button.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-function Button({name, onClick,...props}) {
+function Button({
+  name, 
+  onClick,
+  bgColor = 'bg-indigo-600',
+  textColor= 'text-white',
+  className = '' ,
+  ...props
+}) {
   
   return (
     <button
-        className={`bg-indigo-600 text-white font-[Poppins] py-2 px-5 rounded md:ml-8 hover:bg-indigo-400 duration-500`}
+        className={`${bgColor} ${textColor} ${className} font-[Poppins] py-2 px-5 rounded md:ml-8 hover:bg-indigo-400 duration-500`}
         onClick={onClick}
         {...props}
     >
