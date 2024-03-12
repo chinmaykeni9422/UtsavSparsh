3c94727dcbd903779ed6411f340e0a026b533730
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-12 11:27:17 +0530

    simple identation fixed

--- Client/src/components/Utils/Input.jsx
+++ Client/src/components/Utils/Input.jsx
@@ -1,33 +1,30 @@
 import React, { useId } from 'react'
 
-const InputTaker=React.forwardRef(function InputTaker({
+const InputTaker = React.forwardRef(function InputTaker({
     label,
     type="text",
     className="",
     ...props
-
 },ref){
 
-const id=useId();
+  const id = useId();
 
 return(
     <>
       <div className='w-full'>
 
-        {label && <label className='inline-block mb-1 pl-1' htmlFor={id}>
-
-        {label}
-          
-        </label>}
+        {label && 
+          <label className='inline-block mb-1 pl-1' htmlFor={id}>
+            {label}
+          </label>
+        }
 
         <input
-
           type={type}
           className={`px-3 py-3 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-80 ${className}`}
           ref={ref}
           {...props}
           id={id}
-
         />
 
       </div>
