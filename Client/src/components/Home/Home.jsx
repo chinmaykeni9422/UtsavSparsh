59e1de7cfbb5938a2b3c4c94d4c1552824d6305c
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-12 21:41:20 +0530

    home page welcoome content

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -3,9 +3,23 @@ import React from 'react'
 function Home() {
   return (
     <>
-      <h1 className="bg-green-300 text-3xl text-center">
-        Home
-      </h1>
+      <div className="text-3xl m-auto text-center h-screen">
+
+        <div className='my-8 p-3'>
+
+          <h1 className='uppercase text-sm sm:text-5xl font-[Poppins] text-violet-500'>
+            elevate your events with personalized <span className='text-blue-400 font-serif'>invitations</span>
+          </h1>
+
+          <h4 className='font-mono text-sm sm:text-3xl text-violet-300 uppercase mt-6 sm:text-violet-400'>
+            your one-stop destination for seamless event invitations
+          </h4>
+
+        </div>
+
+        {/* slider */}
+
+      </div>
     </>
   )
 }
