7ee0b3a5fb9457a49c22de6311b0a8877749a089
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-13 19:04:24 +0530

    image card

---
+++ Client/src/components/Carousel/ImgCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function ImgCard({link}) {
+  return (
+    <>
+        <div className='md:h-[350px]  bg-black rounded-xl'>
+            <img className='rounded-xl  h-full' src={link} alt="" />
+        </div>
+    </>
+  )
+}
+
+export default ImgCard
