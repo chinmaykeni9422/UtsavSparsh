06bf93914a52367d985f881c69f5d318e2786f12
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-14 20:31:06 +0530

    styled input commponent

--- Client/src/components/Contact/Contact.jsx
+++ Client/src/components/Contact/Contact.jsx
@@ -12,59 +12,81 @@ function Contact() {
   }
   return (
     <>
-      <div className='mx-5 px-1 flex flex-col'  >
-        <h1 className='text-2xl  text-center my-5'>Contact Us</h1>
 
+      <div className='flex h-[40.25rem]  sm:flex-row justify-center items-center'  >
 
-        <form onSubmit={handleSubmit(contactUsSubmit)}>
+        <div className='w-full h-full m-2 sm:block  hidden'>
+          <img className='mt-1 ml-2 -sm shadow-gray-400 rounded-lg w-full  h-full object-cover object' src="https://img.freepik.com/free-vector/flat-customer-support-illustration_23-2148899114.jpg?w=740&t=st=1710418746~exp=1710419346~hmac=a8495f74d9ff7585a4f70334e7df94a1ddc19c549b444614260965469475bbae" alt="" />
+        </div>
 
-          <div className='grid  sm:grid-cols-2 '>
-            <div>
-              <h2>For More Enquiries</h2>
-              <InputTaker
-                type="text"
-                placeholder="Enter Your Name"
-                {...register("name", {
-                  required: true,
+        <div className='flex flex-col items-center justify-center w-full bg-white'>
 
-                })}
-                className="my-2"
-              />
-              <InputTaker
-                type="email"
-                placeholder="Enter Your Email"
-                {...register("email", {
-                  required: true,
+          <h1 className='text-lg sm:text-3xl mb-1 uppercase text-blue-500 font-serif'>
+            Contact Us
+          </h1>
 
-                })}
-                className="my-2"
-              />
-              <InputTaker
-                type="number"
-                placeholder="Enter Your Mobile Number"
-                {...register("mobile", {
-                  required: true,
-                  maxLength: 10,
+          <p className='text-md sm:text-md text-blue-400 font-serif mb-3'>
+            For More Enquiries
+          </p>
 
-                })}
-                className="my-2"
-              />
-            </div>
-            <div>
-            <textarea 
-            {...register("message",{required:true})}
-            name="message" id="" cols="30" rows="10"
-            placeholder='Write Your Message'>
-              
-            </textarea>
-              <div className='my-2'>
-                <Button type="submit" name="Submit" />
+          <form onSubmit={handleSubmit(contactUsSubmit)}>
+
+            <div className=' flex flex-col justify-center items-center'>
+
+              <div>
+
+                <InputTaker
+                  type="text"
+                  placeholder="Enter Your Name"
+                  {...register("name", {
+                    required: true,
+
+                  })}
+                  className="my-2"
+                />
+
+                <InputTaker
+                  type="email"
+                  placeholder="Enter Your Email"
+                  {...register("email", {
+                    required: true,
+
+                  })}
+                  className="my-2 "
+                />
+
+                <InputTaker
+                  type="text"
+                  placeholder="Enter Your Mobile Number"
+                  {...register("mobile", {
+                    required: true,
+                    maxLength: 10,
+                  })}
+                  className="my-2"
+                />
+
               </div>
+
+              <div className='w-full flex flex-col items-center'>
+
+                <textarea className='focus:outline-none border border-gray-300 w-full my-2 p-3'
+                {...register("message",{required:true})}
+                name="message" id="" cols="30" rows="10"
+                placeholder='Write Your Message'>
+
+                </textarea>
+        
+                <div className='my-2'>
+                  <Button type="submit" name="Submit" />
+                </div>
+
+              </div>
+
             </div>
-          </div>
 
+          </form>
 
-        </form>
+        </div>
 
       </div>
 
