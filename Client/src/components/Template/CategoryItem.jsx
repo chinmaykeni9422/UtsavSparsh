c18bde74c961a9277fdab070107fbfef358967be
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-16 20:48:13 +0530

    Categoryitem component

---
+++ Client/src/components/Template/CategoryItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function CategoryItem({image, title}) {
+  return (
+    <>
+
+        <div className='p-5'>
+
+            <div className="border border-gray-300 rounded-lg overflow-hidden">
+                <img className='w-[550px] h-[250px] object-cover' src={image} alt="" />
+            </div>
+
+            <div className='hover:text-blue-600 font-serif mt-3 text-center'>
+                <p>{title}</p>
+            </div>
+
+        </div>
+
+    </>
+  )
+}
+
+export default CategoryItem
