564b18e35be895e7c91edc09c18663fe4bcdda03
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-17 13:03:40 +0530

    Category item component

--- Client/src/components/Template/CategoryItem.jsx
+++ Client/src/components/Template/CategoryItem.jsx
@@ -4,13 +4,13 @@ function CategoryItem({image, title}) {
   return (
     <>
 
-        <div className='p-5'>
+        <div className='p-5 hover:text-blue-600'>
 
             <div className="border border-gray-300 rounded-lg overflow-hidden">
                 <img className='w-[550px] h-[250px] object-cover' src={image} alt="" />
             </div>
 
-            <div className='hover:text-blue-600 font-serif mt-3 text-center'>
+            <div className=' font-serif mt-3 text-center'>
                 <p>{title}</p>
             </div>
 
