8d74d344d3f0509e628646c747158243f03ccb10
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-18 21:10:15 +0530

    .

---
+++ Client/src/assets/allData.js
@@ -0,0 +1,53 @@
+// corousel dummy images 
+import greenImg from '../assets/plainGreen.jpg'
+
+export const images = [
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  },
+  {
+    img : greenImg
+  }
+] ;
+
+
+
+// category img data
+import babyImg from '../assets/babyShower.jpg'
+import birthdayImg from '../assets/birthday.jpg'
+import engImg from '../assets/engegment.jpg'
+import wedImg from '../assets/wedding.jpg'
+
+export const imgData = [
+    {
+      title: 'wedding Invitation' ,
+      img : wedImg
+    },
+    {
+      title: 'baby shower Invitation' ,
+      img : babyImg
+    },
+    {
+      title: 'birthday Invitation' ,
+      img : birthdayImg
+    },
+    {
+      title: 'engagement Invitation' ,
+      img : engImg
+    }
+  ]
