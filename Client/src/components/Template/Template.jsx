86adfebbeb93cc8a9a6c83992568346ac5efcca7
chinmay keni <137027001+chinmaykeni9422@users.noreply.github.com>
2024-03-21 20:34:41 +0530

    .

--- Client/src/components/Template/Template.jsx
+++ Client/src/components/Template/Template.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CategoryItem from './CategoryItem';
 import { NavLink } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
